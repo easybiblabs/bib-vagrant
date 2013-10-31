5264f1fef95f982fe360169f6e52c356f1f10b3f
till <till@php.net>
2013-10-31 16:31:19 +0100

    Enhancement: add Config object
     * add a config object
     * add a test to cover it

---
+++ lib/bib/vagrant/config.rb
@@ -0,0 +1,64 @@
+require 'yaml'
+
+module Bib
+  module Vagrant
+    class Config
+
+      @@home_dir = nil
+      @@verbose = true
+
+      def initialize(home = "~", verbose = true)
+        @@home = home
+        @@verbose = verbose
+      end
+
+      def get
+
+        vagrantconfig = get_defaults
+
+        begin
+          localconfigfile = File.open(get_path, 'r')
+          vagrantconfig.merge!(YAML.load(localconfigfile.read))
+        rescue Errno::ENOENT
+          puts 'WARNING: No vagrant user-config found, using default cookbook path' if @@verbose
+          create(get_path, vagrantconfig)
+        end
+
+        return vagrantconfig
+      end
+
+      def has?
+        File.exists?(get_path)
+      end
+
+      private
+      def create(localconfigpath, vagrantconfig)
+        begin
+          FileUtils.mkdir_p(File.dirname(localconfigpath))
+          File.open(localconfigpath, 'w+') do |file|
+            file.write( vagrantconfig.to_yaml )
+            puts "INFO: Created default vagrant user-config in #{localconfigpath}" if @@verbose
+            puts "INFO: You probably want to fix the path to the cookbooks in this file." if @@verbose
+          end
+        rescue
+          puts "WARNING: Unable to create default #{localconfigpath} - please do it manually." if @@verbose
+        end
+      end
+
+      def get_path
+        File.expand_path("#{@@home}/.config/easybib/vagrantdefault.yml")
+      end
+
+      def get_defaults
+        {
+          "nfs" => false,
+          "cookbook_path" => '~/Sites/easybib/cookbooks',
+          "chef_log_level" => 'debug',
+          "additional_json" => '{}',
+          "gui" => false
+        }
+      end
+
+    end
+  end
+end
---
+++ test/config_test.rb
@@ -0,0 +1,32 @@
+require 'bib/vagrant/config'
+
+class ConfigTest < Minitest::Test
+
+  @@fixture_dir = nil
+
+  def setup
+    @@fixture_dir = File.dirname(__FILE__) + "/fixtures"
+
+    FileUtils.rm_rf @@fixture_dir if File.exists?(@@fixture_dir)
+    FileUtils.mkdir @@fixture_dir
+
+  end
+
+  def test_config
+
+    c = Bib::Vagrant::Config.new(@@fixture_dir, false)
+    assert_equal(false, c.has?)
+
+    vagrant_config = c.get
+
+    assert(File.exists?("#{@@fixture_dir}/.config/easybib/vagrantdefault.yml"))
+    assert_kind_of(Hash, vagrant_config)
+
+    assert_equal(false, vagrant_config["nfs"])
+    assert_equal("~/Sites/easybib/cookbooks", vagrant_config["cookbook_path"])
+    assert_equal("debug", vagrant_config["chef_log_level"])
+    assert_equal("{}", vagrant_config["additional_json"])
+    assert_equal(false, vagrant_config["gui"])
+
+  end
+end
