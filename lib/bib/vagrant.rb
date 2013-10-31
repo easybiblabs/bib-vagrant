9f0d5e1d4b95a9f0eb4f5c9de231f90efe30e3fb
till <till@php.net>
2013-10-31 19:22:46 +0100

    Enhancement: add a bin-wrapper

--- lib/bib/vagrant.rb
+++ lib/bib/vagrant.rb
@@ -1,7 +1,31 @@
 require "bib/vagrant/version"
+require "bib/vagrant/config"
+require "thor"
 
-module Bib
-  module Vagrant
-    # Your code goes here...
+class BibVagrant < Thor
+  package_name 'bib-vagrant'
+
+  desc 'show', 'show configuration settings'
+  def show
+    config = Bib::Vagrant::Config.new
+    raise "No configuration, run `bib-vagrant setup`!" unless config.has?
+
+    puts "Your configuration is located in: #{config.get_path}"
+    puts ""
+
+    config.get.each do |config_key,config_value|
+      puts "#{config_key}: #{config_value}"
+    end
   end
+
+  desc 'setup', 'setup local configuration with default values'
+  def setup
+    config = Bib::Vagrant::Config.new
+    raise "Your configuration is already created: #{config.get_path}" if config.has?
+
+    config.get
+
+    puts "Configuration created in #{config.get_path}!"
+  end
+
 end
