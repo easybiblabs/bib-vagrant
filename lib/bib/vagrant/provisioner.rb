83a97017da7dc8e34d46c0e72ecb6d39bcd2f052
Joel Gilley <joel.gilley@ImagineEasy.com>
2015-09-21 14:27:12 -0400

    Working on composer

--- lib/bib/vagrant/provisioner.rb
+++ lib/bib/vagrant/provisioner.rb
@@ -30,44 +30,54 @@ class BibConfigurePlugin < Vagrant.plugin('2')
         bib_config_values = bib_config.get
         bib_config.validate!(bib_config_values)
 
+        # sneaky fix to "stdin: is not a tty" noise
+        send_command("sudo sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile")
+
         if bib_config_values.include?('npm_auth')
-          npmrc_set_or_delete('_auth',bib_config_values['npm_auth'])
+          npmrc_set('_auth',bib_config_values['npm_auth'])
         else
           @machine.ui.error("Missing npm_auth value in config")
         end
 
         if bib_config_values.include?('npm_registry')
-          npmrc_set_or_delete('registry',bib_config_values['npm_registry'])
+          npmrc_set('registry',bib_config_values['npm_registry'])
         else
           @machine.ui.error("Missing npm_registry value in config")
         end
 
         if bib_config_values.include?('npm_email')
-          npmrc_set_or_delete('email',bib_config_values['npm_email'])
+          npmrc_set('email',bib_config_values['npm_email'])
         else
           @machine.ui.error("Missing npm_email value in config")
         end
 
         if bib_config_values.include?('npm_always-auth')
-          npmrc_set_or_delete('always-auth',bib_config_values['npm_always-auth'])
+          npmrc_set('always-auth',bib_config_values['npm_always-auth'])
         else
           @machine.ui.error("Missing npm_always-auth value in config")
         end
 
-        # send_command('npm config ls')
-        # send_command('npm -d view npm')
+        # if bib_config_values.includ?('composer_github_token')
+        #   composer_set('github-oauth.github.com', bib_config_values['composer_github_token'])
+        # else
+        #   @machine.ui.error("Missing composer_github_token value in config")
+        # end
   
         # Now the machine is up again, perform the necessary tasks.
         @machine.ui.info('bib-vagrant config complete...')
       end
 
-      # composer config -g github-oauth.github.com <oauthtoken>
+      # def composer_set(key, value)
+      #   command = "sudo composer config -g #{key} #{value}"
+      #   send_command(command)
+      # end
 
-      def npmrc_set_or_delete(key, value)
+      def npmrc_set(key, value)
         command = "sudo npm -g "
         if value
           command << "set #{key} #{value}"
         else
+          # if value is null assume a delete
           command << "delete #{key}"
           # fix for npmrc key not existing
           send_command("sudo npm -g set #{key} GNDN")
